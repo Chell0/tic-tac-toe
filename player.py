55bc16fb1e608db30e47076630c115733219d476
Gabriel Machel O <machelgabriel@gmail.com>
2021-01-19 16:55:29 +0300

    Edit th input section in player.py

--- player.py
+++ player.py
@@ -30,7 +30,7 @@ class HumanPlayer(Player):
         valid_square = False
         val = None
         while not valid_square:
-            square = input(self.letter + '\'s turn. Input move (0-9):')
+            square = input(self.letter + '\'s turn. Input move (0-8):')
             #  we're going to check that this is a correct value by trying to cast
             #  it to an integer, and if it's not, then we say invalid
             #  if that spot is not available on the board, we also say it's invalid
